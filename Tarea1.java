99d4f1704d986bb4186636810c51dbc95af76e8d
SebastianZs <60480495+SebastianZs@users.noreply.github.com>
2020-02-04 21:33:31 -0500

    Add files via upload

---
+++ Tarea1.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.tarea1p3szs;
+
+/**
+ *
+ * @author Sebas
+ */
+public class Tarea1 {
+      public static void main(String[] args) {
+    double k=0;
+    for (double i=1;i<=100000000;i+=2){
+    k+=i;    
+}
+    System.out.println(k);
+}
+}
